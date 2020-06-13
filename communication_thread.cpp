07f34ec061fbe9009c13b8ede7837852c6ade165
fijka <57313936+fijka@users.noreply.github.com>
2020-06-13 17:25:03 +0200

    Add C++ files

---
+++ communication_thread.cpp
@@ -0,0 +1,38 @@
+#include "main.h"
+#include "communication_thread.h"
+
+/* wątek komunikacyjny: odbieranie wiadomości i reagowanie na nie */
+void *startCommunicationThread(void *ptr)
+{
+    MPI_Status status;
+    int is_message = FALSE;
+    packet_t packet;
+
+    // ZLECENIODAWCA: reakcja na odebrane wiadomości
+    if (rank == 0) {
+        while (TRUE) {
+            debug("[%d] czekam na wiadomość", rank);
+            MPI_Recv(&packet, 1, MPI_PACKET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+            switch (status.MPI_TAG) {
+                // ...
+                default:
+                    break;
+            }
+        }
+    }
+    // PROFESJONALISTA: reakcja na odebrane wiadomości
+    else {
+        state = mission_wait;
+        while (TRUE) {
+            debug("[%d] czekam na wiadomość", rank);
+            MPI_Recv( &packet, 1, MPI_PACKET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+            switch (status.MPI_TAG) {
+                // ...
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ main_thread.cpp
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "main_thread.h"
+
+/* wątek główny: praca zleceniodawcy oraz profesjonalistów */
+void mainLoop()
+{
+    // ZLECENIODAWCA
+    if (rank == 0) {
+        while (TRUE) {
+            // generowanie zleceń i informaowanie o nich
+
+        }
+    }
+    // PROFESJONALISTA
+    else {
+        while (TRUE) {
+            // ubieganie się o zasoby, wskrzeszanie
+
+        }
+    }
+}
